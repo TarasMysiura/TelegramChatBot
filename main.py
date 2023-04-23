3dbc2644610f6aea66269e7ce11be044dd0ba123
TarasMysiura <tarasmysiura@gmail.com>
2023-04-23 08:56:05 +0300

    add

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-BOT_TOKEN = ""
+BOT_TOKEN = "5863212826:AAHfHUxMQlImc98FwRkjwa4aRefArGCmjbY"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+     await update.message.reply_text("Вітаю у моєму боті")
+
+
+def run():
+    aplication = ApplicationBuilder().token(BOT_TOKEN).build()
+    
+    aplication.add_handler(CommandHandler("start", start))
+    aplication.add_handler(CommandHandler("help", start))
+
+    aplication.run_polling()
+
+
+if __name__ == "__main__":
+    run()
